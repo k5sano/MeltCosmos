ec39405f8c5e04dbd8aa93951ad401a688af9c4c
k5sano <82030004+k5sano@users.noreply.github.com>
2026-03-09 13:42:10 +0900

    chore(handoff): update Source/PluginEditor.h from Genspark

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,17 +1,41 @@
 #pragma once
 
-#include <juce\_audio\_processors/juce\_audio\_processors.h> #include "PluginProcessor.h"
+#include <juce\_audio\_processors/juce\_audio\_processors.h> #include "PluginProcessor.h" #include "DSP/LevelMeter.h"
 
-class AvalancheEditor : public juce::AudioProcessorEditor { public: explicit AvalancheEditor(AvalanchePlugin&); ~AvalancheEditor() override = default;
+class AvalancheEditor : public juce::AudioProcessorEditor, private juce::Timer { public: explicit AvalancheEditor(AvalanchePlugin&); ~AvalancheEditor() override;
 
 ```
 void paint(juce::Graphics&) override;
 void resized() override;
 ```
 
-private: AvalanchePlugin& processor; juce::GenericAudioProcessorEditor genericEditor;
+private: void timerCallback() override;
 
 ```
+AvalanchePlugin& processor;
+juce::GenericAudioProcessorEditor genericEditor;
+
+// Cached meter levels for painting
+struct MeterState {
+    float peakDb = -100.0f;
+    float rmsDb  = -100.0f;
+    float peakHold = -100.0f;
+    float holdTimer = 0.0f;
+};
+
+static constexpr int kNumMeters = 6;
+MeterState meters_[kNumMeters] = {};
+static constexpr const char* meterNames_[kNumMeters] = {
+    "INPUT", "DELAY", "REV.IN", "REV.OUT",
+    "X-FEED", "OUTPUT"
+};
+
+void drawMeter(juce::Graphics& g, juce::Rectangle<int> bounds,
+               const char* label, const MeterState& state);
+
+static juce::Colour levelToColour(float db);
+static float dbToX(float db, float width);
+
 JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AvalancheEditor)
 ```
 
